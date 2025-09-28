d9c098bb9891c1166fee10bd4f4d25f243f8c974
olamide-abegunde-cko1 <olamide.abegunde@checkout.com>
2025-09-28 09:41:05 +0100

    feat(frontend): add global header and navigation links

---
+++ frontend/app/components/Header.tsx
@@ -0,0 +1,15 @@
+import Link from "next/link";
+
+export default function Header() {
+    return(
+        <header className="bg-gray-100 shadow p-4 flex justify-between items-center">
+            <h1 className="text-xl font-bold">
+                <Link href={"/"}>Chair</Link>
+            </h1>
+            <nav className="space-x-4">
+                <Link href={"/appointments"} className={"hover:underline"}>Appointments</Link>
+                <Link href={"/stylists"} className={"hover:underline"}>Stylists</Link>
+            </nav>
+        </header>
+    )
+}
